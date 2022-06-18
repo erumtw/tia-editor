4f5e0b8031cdf2b4f5ba8b998337e4f9177406cb
amreemaru <heartof.effort@gmail.com>
2022-06-18 10:59:31 +0700

    function added

--- src/Editor.java
+++ src/Editor.java
@@ -1,9 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javafx.application.Application;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class Editor {
+    String textData;
 
     public Editor () {
 
@@ -24,17 +35,57 @@ public class Editor {
     //    else // in case text area's not empty then
                // check if saved or nah? by text file == text area ? 
                // case true: Open the text file  
-               // case false: warning to save first 
+               // case false: warning to save first
+        TiaEditor.fChooser.setTitle("Open");
+        TiaEditor.textFile = TiaEditor.fChooser.showOpenDialog(TiaEditor.mainStage); 
+        
+        if (TiaEditor.textFile != null) {
+
+            try {
+                byte[] readText = Files.readAllBytes(Paths.get(TiaEditor.textFile.getAbsolutePath())); 
+                textData = new String(readText);
+                TiaEditor.textArea.setText(textData);
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } 
+            
+        else    
+            TiaEditor.textArea.setText("unselected");
     }
 
     public void saveOption() {
         // check if the file's from open option or already have a file as a same path
         // case true: then save the file
         // case false: involve saveAsOption
+            try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(TiaEditor.textFile))) {
+                bWriter.write(TiaEditor.textArea.getText());
+    
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        
+        
     }
 
     public void saveAsOption() {
         // save the file that can select the path by fileChooser
+        TiaEditor.fChooser.setTitle("Save as");
+        TiaEditor.textFile = TiaEditor.fChooser.showSaveDialog(TiaEditor.mainStage);
+
+        try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(TiaEditor.textFile))) {
+            bWriter.write(TiaEditor.textArea.getText());
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
     }
 
 
--- src/TiaEditor.java
+++ src/TiaEditor.java
@@ -1,6 +1,5 @@
-import java.io.File;
 
-import javax.lang.model.util.ElementScanner14;
+import java.io.File;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -11,24 +10,29 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+
 public class TiaEditor extends Application {
+    protected Editor editor = new Editor();
     protected static File textFile;
     protected static Alert userAlert;
-    Editor editor = new Editor();
     protected static TextArea textArea = new TextArea();
-
+    protected static FileChooser fChooser = new FileChooser();
+    protected static String filePathString;
+    protected static Stage mainStage;
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage mainStage) throws Exception {
         
         MenuBar menuBar = new MenuBar();
         Menu fileMenu = new Menu("File");
-
+        
+        fChooser.setInitialFileName(".txt");
         String[] fileOption = {
             "New",
             "Open",
@@ -44,10 +48,16 @@ public class TiaEditor extends Application {
                     editor.newOption();
                 }
                 else if ("Open".equals(addMenuItem.getText())) {
-                    editor.openOption();
+                    if (textFile == null) 
+                        editor.openOption();
+                    else 
+                        editor.saveAsOption();
                 }
                 else if ("Save".equals(addMenuItem.getText())) {
-                    editor.saveOption();
+                    if (textFile != null && textFile.exists())
+                        editor.saveOption();
+                    else 
+                        editor.saveAsOption();
                 }
                 else{
                     editor.saveAsOption();
@@ -60,10 +70,12 @@ public class TiaEditor extends Application {
 
         vBox.getChildren().addAll(menuBar, textArea);
         
-        Scene scene = new Scene(vBox);
-        stage.setTitle("TiaEditor");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(vBox, 500, 500);
+        mainStage.setTitle("TiaEditor");
+        mainStage.setScene(scene);
+        mainStage.show();
 
     }
+
+    
 }
